5efe4a3da1121ddccac380e7bcc9412076a30786
xjx <644233452@qq.com>
2018-08-19 22:45:27 +0800

    add some code

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,34 +1,34 @@
 const app = getApp()
 
 Page({
-  data: {
-    hasUserInfo: false,
-    userInfo: {},
-    src: "../../images/profile.jpg"
-  },
-  onLoad: function() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    data: {
+        hasUserInfo: false,
+        userInfo: {},
+        src: "../../images/profile.jpg"
+    },
+    onLoad: function() {
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        }
+    },
+    bindgetuserinfo: function(e) {
+        console.log("bindgetuserinfo: ", e.detail);
+        app.globalData.userInfo = e.detail.userInfo;
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
         })
-      }
     }
-  },
-  bindgetuserinfo: function(e) {
-    console.log("bindgetuserinfo: ", e.detail);
-    app.globalData.userInfo = e.detail.userInfo;
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })

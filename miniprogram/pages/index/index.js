86979a45de920ca0c90641b2efa15583b780abef
xjx <644233452@qq.com>
2018-08-20 22:40:32 +0800

    add home page

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,34 +1,64 @@
 const app = getApp()
 
 Page({
-    data: {
-        hasUserInfo: false,
-        userInfo: {},
-        src: "../../images/profile.jpg"
-    },
-    onLoad: function() {
-        if (app.globalData.userInfo) {
-            this.setData({
-                userInfo: app.globalData.userInfo,
-                hasUserInfo: true
-            })
-        } else {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
-            }
-        }
-    },
-    bindgetuserinfo: function(e) {
-        console.log("bindgetuserinfo: ", e.detail);
-        app.globalData.userInfo = e.detail.userInfo;
+  data: {
+    hasUserInfo: false,
+    userInfo: {},
+    src: "../../images/profile.jpg",
+    createRoomNodes: [{
+      name: 'a',
+      attrs: {
+        class: 'draw-home-create-text',
+        style: ''
+      },
+      children: [{
+        type: 'text',
+        text: '开始创建 >'
+      }]
+    }],
+    startGameNodes: [{
+      name: 'a',
+      attrs: {
+        class: 'draw-home-create-text',
+        style: ''
+      },
+      children: [{
+        type: 'text',
+        text: '立即开始 >'
+      }]
+    }],
+    createRoomSrc: '../../images/1.png',
+    startGameSrc: '../../images/2.png'
+  },
+  onLoad: function() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
         this.setData({
-            userInfo: e.detail.userInfo,
-            hasUserInfo: true
+          userInfo: res.userInfo,
+          hasUserInfo: true
         })
+      }
     }
+  },
+  bindgetuserinfo: function(e) {
+    console.log("bindgetuserinfo: ", e.detail);
+    app.globalData.userInfo = e.detail.userInfo;
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  createRoom: function() {
+    console.log("createRoom");
+  },
+  startGame: function() {
+    console.log('startGame');
+  }
 })
